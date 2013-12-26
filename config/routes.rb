ec424fcdab7abb21d61a946226228790db8ea162
Mike Funderburk <mtfunderburk@gmail.com>
2013-12-26 02:24:31 -0500

    Created Home, About, and Mike Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+  get "Mike" => "pages#Mike"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
